9f49eae88fa189be62ee926d9d47bdca84600309
Luce Michaelis <lucerorocks17@gmail.com>
2018-02-12 23:29:47 +0000

    I made my picture work

--- js/script.js
+++ js/script.js
@@ -25,22 +25,23 @@ var mySong = {
 
 var myPlayList = [
 	{
-		"title":"24K Magic",
-		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+	"title":"Downtown",
+	"artist":"Anitta ft. Jbalvin",
+	"mp3Url":"https://www.youtube.com/watch?v=qu5l42GFgFc",
+	"imageUrl":"https://i.scdn.co/image/fba406f05dd5f28dc81873ef7f798149f177af02",
+		
 	},
 	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"title":"La Modelo",
+		"artist":"Ozuna ft Cardi B",
+		"mp3-url":"https://www.youtube.com/watch?v=EH2XAe7ayy0",
+		"image-url":"https://is5-ssl.mzstatic.com/image/thumb/Music128/v4/b6/70/4c/b6704c61-a452-e495-d0fc-1aabc2ed0691/697691884097.jpg/268x0w.jpg",
 	},
 	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"title":"Dura",
+		"artist":"Daddy Yankee",
+		"mp3-url":"https://www.youtube.com/watch?v=xaDUws6snrQ",
+		"image-url":"http://4.bp.blogspot.com/-MOndwp8u93g/WmDhWAfRuTI/AAAAAAAAADs/aVNxtE_hExEx2woDBixTkt838IcSG3p0ACK4BGAYYCw/s1600/Dura%2B-%2BSingle.jpg",
 	}
 
 ]
@@ -49,10 +50,11 @@ var myPlayList = [
 
 // DOCUMENT READY FUNCTION
 $( document ).ready(function() {
+	    $('body').append(`<img src="${mySong.imageUrl}">`);
     $('body').append(`<p> Title: ${mySong.title} </p>`);
     $('body').append(`<p> Artist: ${mySong.artist} </p>`);
     $('body').append(`<p>mp3-url: <a href= "${mySong.mp3Url}">downtown</a></p>`);
-    $('body').append(`<p>mp3-url ${mySong.imageUrl}</p>`);
+
 
 });
 
